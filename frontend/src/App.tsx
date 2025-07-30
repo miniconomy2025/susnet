36488b491b4832571e7c8c7f600923a82d339aec
Shailyn Ramsamy Moodley <shailyn.moodley@bbd.co.za>
2025-07-30 23:48:50 +0200

    auth and header components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,27 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import Account from './pages/Account';
-import Signup from './pages/Signup';
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
+import Home from './pages/Home'
+import Account from './pages/Account'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import HeaderComponent from './Components/HeaderComponent/HeaderComponent'
 
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Signup />} />
-				<Route path="/home" element={<Home />} />
-				<Route path="/account" element={<Account />} />
-			</Routes>
-		</Router>
-	);
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <div className="page">
+        <HeaderComponent />
+        <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
