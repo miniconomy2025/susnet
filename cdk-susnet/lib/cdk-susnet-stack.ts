f52df710f117f6e287e3b6afabc3658a62f4d83e
Mick Holmes <joshua.holmes@vcontractor.co.za>
2025-07-30 13:23:55 +0200

    test

--- cdk-susnet/lib/cdk-susnet-stack.ts
+++ cdk-susnet/lib/cdk-susnet-stack.ts
@@ -32,6 +32,7 @@ export class CdkSusnetStack extends cdk.Stack {
 
     // Step 3: Attach the desired policies
     githubActionsRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('SecretsManagerReadWrite'));
+    githubActionsRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AWSCloudFormationFullAccess'));
 
     //CERT STUFF
     const hostedZone = new HostedZone(this, 'hostedZone', {
