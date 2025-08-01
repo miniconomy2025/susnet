4534a0205fa63027c3571f16d87fc5a9582940dc
Mick Holmes <joshua.holmes@vcontractor.co.za>
2025-08-01 23:13:39 +0200

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,11 @@ module.exports = {
       name: 'deno-backend',
       script: './start-backend.sh',
       interpreter: '/bin/bash', // ✅ Use bash to run .sh scripts
-      watch: true
     },
     {
       name: 'vite-frontend',
       script: './start-frontend.sh',
       interpreter: '/bin/bash', // ✅ Use bash to run .sh scripts
-      watch: true
     }
   ]
 }
