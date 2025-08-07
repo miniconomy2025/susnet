4648211bb75ab523501b81452e79a1f266d711d7
Gregory Conroy <gregory.conroy@bbd.co.za>
2025-08-07 20:05:08 +0200

    Following buttons show where required

--- frontend/src/models/Feed.ts
+++ frontend/src/models/Feed.ts
@@ -3,16 +3,16 @@ import { PostData, Res_Feed, Res_getActor } from '../../../types/api';
 export interface BannerProps {
 	title: string;
 	displayImage?: string;
-	membershipStatus?: MembershipStatus;
+	initialIsFollowing?: boolean;
 	onCreatePost?: () => void;
 	onCreateSub?: () => void;
-	onSetMembershipClick?: (status: MembershipStatus) => void;
 	onSettingsClick?: () => void;
 }
 
 export interface FeedContainerProps {
 	bannerProps: BannerProps;
 	availablePosts?: PostData<'full'>[];
+	showCardFollowButton: boolean;
 	onLoadPosts: (cursor: string) => Promise<Res_Feed | undefined>;
 	onRefresh: () => Promise<void>;
 }
